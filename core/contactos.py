2dd64ca62e1fc8ba7e1d7add69308927b28e12c6
Yokasta <terreoacevedoe@gmail.com>
2025-07-28 11:23:42 -0400

    Ahora se ha implementado un crud basico funcional nada muy fancy

--- core/contactos.py
+++ core/contactos.py
@@ -1,6 +1,7 @@
 from core import writer
 import re
 import csv
+from tabulate import tabulate
 
 
 
@@ -40,6 +41,59 @@ def guardar_contacto():
                           }
     return writer.output(resgistar_contacto)
 
+def mostrar_contactos():
+    file_paht  = 'csv/data.csv'
+    with open(file_paht, mode="r", newline="") as f:
+        leyendo = csv.DictReader(f)
+        filas = [lee for lee in leyendo]
+        print(tabulate(filas, headers='keys', tablefmt='grid'))
+
+def editar_contacto():
+    file_paht  = 'csv/data.csv'
+    print("Cual contacto quiere Editar?")
+    with open(file_paht, mode='r', newline="") as f :
+        leyendo_datos = csv.DictReader(f)
+        lista_editable = [lee for lee in leyendo_datos]
+        print(f" contactos editables: {tabulate(lista_editable, headers='keys', tablefmt='grid')}")
+    
+    editando_contacto = input("Seleccione el ID a editar: ")
+    contacto_filtrado = [contacto for contacto in lista_editable if contacto['id'] == editando_contacto]
+    if not contacto_filtrado:
+        print("No se encuntra ningun usuario con este ID")
+    else:
+        print(f"Editando contacto : {tabulate(contacto_filtrado, headers='keys', tablefmt='grid')}")
+        for dato in contacto_filtrado:
+            editar_dato = input(f"Cual dato quiere camabiar? ({','.join(dato.keys())}): ").lower()
+            if not editar_dato:
+                print("Debe especificar el dato a cambiar")
+            else: 
+                if editar_dato in dato:
+                    nuevo_dato = input(f"Ingrese el nuevo valor para {editar_dato}: ")
+                    dato[editar_dato] = nuevo_dato
+
+                    with open(file_paht, mode='w', newline='') as f:
+                        campos = ['id', 'nombre', 'email', 'telefono']
+                        escritor = csv.DictWriter(f, fieldnames=campos)
+                        escritor.writeheader()
+                        escritor.writerows(lista_editable)
+                        print(f" Actualizado correctamente {tabulate(lista_editable, headers='keys', tablefmt='grid')}")
+
+def eliminar_contacto(id):
+    file_paht  = 'csv/data.csv'
+    with open(file_paht, mode="r", newline="") as f:
+        leyendo  = csv.DictReader(f)
+        print(f"\nEste es tu id: {id}")
+        contacto_encontrado = [lee for lee in leyendo if lee['id'] != id]
+    
+    with open(file_paht, mode='w', newline="") as f:
+        
+        campos = ['id', 'nombre', 'email', 'telefono']
+        escritor = csv.DictWriter(f, fieldnames=campos)
+        escritor.writeheader()
+        escritor.writerows(contacto_encontrado)
+        print("Esto son los contactos restantes")
+        print(f"{tabulate(contacto_encontrado, headers='keys', tablefmt='grid')}")
+
 def buscar_contacto(id):
     file_paht  = 'csv/data.csv'
     with open(file_paht, mode="r", newline="") as f:
@@ -49,4 +103,9 @@ def buscar_contacto(id):
         if contacto_encontrado == []:
             print("este id no exsiste")
         else:
-            print(contacto_encontrado)
+            print(tabulate(contacto_encontrado, headers='keys', tablefmt='grid'))
+
+
+
+
+
--- main.py
+++ main.py
@@ -4,14 +4,34 @@ while True:
     print("\nAgendify")
     print("1. Agregar un nuevo contacto")
     print("2. Buscar contacto exisitente")
-    print("3. salir")
+    print("3. Mostrar todos los contactos")
+    print("4. Eliminar contactos")
+    print("5. Editar contactos")
+    print("6. salir")
 
     opcion = input("Escoja una opción: ")
 
     if opcion == '1':
+        print("Agregando un nuevo contaccto:")
         contactos.guardar_contacto()
     elif opcion == '2':
+        print("Buscando Contacto exsitente")
         pedir_id = input("coloque el id que quiere buscar: ")
         contactos.buscar_contacto(pedir_id)
     elif opcion == '3':
-        break
+        print("Mostrnado todos los contactos:")
+        contactos.mostrar_contactos()
+    elif opcion == '4':
+        print("Aqui eliminaremos un contacto")
+        pedir_id = input("coloque el id que quiere buscar: ")
+        contactos.eliminar_contacto(pedir_id)
+        pass
+    elif opcion == '5':
+        while True:
+            contactos.editar_contacto()
+            if input("Desea seguir editando algun contacto? s/n").lower() != "s":
+                break
+
+    elif opcion == '6':
+        print("saliendo de la app")
+        break 
